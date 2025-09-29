496bf6642917828666f07f8b5c6ed9e81b25de31
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2025-09-29 10:23:07 +0200

    Create Start.cs

---
+++ Space_Invaders/Start.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Space_Invaders
+{
+    internal class Start
+    {
+        Texture2D backgroundPicture;
+        Texture2D spriteSheet;
+        Rectangle sourceRec;
+        Vector2 position;
+
+        public void StartBackground(Texture2D backgroundPicture, Texture2D spriteSheet, Vector2 position, Rectangle sourceRec)
+        {
+            this.backgroundPicture = backgroundPicture;
+            this.position = position;
+            this.spriteSheet = spriteSheet;
+            this.sourceRec = sourceRec;
+        }
+        public void Update()
+        {
+            // Any update logic for the start screen can go here
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(backgroundPicture, position, sourceRec, Color.White);
+            sb.Draw(spriteSheet, position, sourceRec, Color.White);
+        }
+    }
+}
